ede17a8da6718dd0fd37da5c1ff7e800e012f7da
EternalHelI <eternalhell@eterneon.net>
2025-10-07 03:18:03 -0700

    First Mod Upload

---
+++ src/main/java/net/eterneon/bloom/BloomDataGenerator.java
@@ -0,0 +1,11 @@
+package net.eterneon.bloom;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BloomDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
